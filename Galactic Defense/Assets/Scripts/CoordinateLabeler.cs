8659a1ba69498ddf60ebdc474cd15b23984014e4
cswitzer <50593813+cswitzer@users.noreply.github.com>
2021-03-25 00:31:06 -0600

    Added world grid
    Added a world grid which has snap restraints (10 units) that enemy can move along smoothly

---
+++ Galactic Defense/Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    // Coordinates come from current position in world space
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates(); // so coordinates will display in game mode
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // script will only execute in play mode
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        // grab information from gameobject (tile which is a parent) pertaining to world coordinate position
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x); // displays increments of 1
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        // set the text for current instance of text mesh to corresponding coordinate
+        label.text = coordinates.x.ToString() + "," + coordinates.y.ToString();
+    }
+
+    // changes the name of this object's instance to its coordinates
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
---
+++ Galactic Defense/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    // Pass in some waypoints that the enemy will follow along
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintWaypointName());
+    }
+
+    IEnumerator PrintWaypointName()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            // move the enemy to the position of the next waypoint
+            transform.position = waypoint.transform.position;
+            // yield means to give up control and come back to me in 'n' seconds
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
